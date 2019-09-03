4baa40d859eded867469e3cf03f1b654e84cadbb
591546487@qq.com <591546487@qq.com>
2019-09-03 15:13:22 +0800

    add 初始化手写页面，增加配置

--- CymHandWritten/CymHandWritten/ViewController.m
+++ CymHandWritten/CymHandWritten/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "CJDrawViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<CJDrawViewControllerDelegate>
+{
+     UIImageView *showImageView;// 显示签名图片
+}
 @end
 
 @implementation ViewController
@@ -17,7 +20,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+    
+    showImageView = [[UIImageView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:showImageView];
+    showImageView.contentMode = UIViewContentModeScaleAspectFit;
 }
 
+#pragma mark - touch self.view
+// 点击页面弹出签名页
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    CJDrawViewController *VC = [CJDrawViewController new];
+    VC.delegate = self;
+    [self presentViewController:VC animated:YES completion:nil];
+    
+}
+#pragma mark - cjdrawVC delegate
+// 签名之后形成图片
+- (void)returnImage:(UIImage *)signImage
+{
+    showImageView.frame = CGRectMake(0, 0, signImage.size.width, signImage.size.height);
+    showImageView.center = self.view.center;
+    showImageView.image = signImage;
+}
 
 @end
